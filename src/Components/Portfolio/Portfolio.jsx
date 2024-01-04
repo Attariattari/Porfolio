2a65c465e6ad131744ed87f714e51d9aa2cf776d
Attariattari <attariattari549@gmail.com>
2024-01-04 19:00:53 +0500

    first commit

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -10,6 +10,14 @@ function Portfolio() {
           <div className="text-lg">Check out our latest creative works</div>
         </div>
       </div>
+      <div className="ProtfolioSection">
+        <div className="ProtfolioButtons">
+          <button className="Buttonfor">All Show</button>
+          <button className="Buttonfor">Mern Stack</button>
+          <button className="Buttonfor">UX / UI Designing</button>
+          <button className="Buttonfor">Backend Projects</button>
+        </div>
+      </div>
     </div>
   );
 }
