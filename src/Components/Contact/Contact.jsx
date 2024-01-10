7e149ace0c29fae3f734f5a7329dd63abb6bb2c9
Attariattari <attariattari549@gmail.com>
2024-01-11 00:15:40 +0500

    first commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Contact.css";
 import { useForm, ValidationError } from "@formspree/react";
 import { toast, ToastContainer } from "react-toastify";
@@ -20,6 +20,13 @@ function Contact() {
   const [errors, setErrors] = useState({});
   const [maxRows, setMaxRows] = useState(5);
   const [state, handleSubmitFormspree] = useForm("xpzvwzeq");
+  useEffect(() => {
+    if (state.succeeded) {
+      toast.success("Form submitted successfully!");
+    } else if (state.errors && state.errors.length > 0) {
+      toast.error("Error submitting form. Please try again.");
+    }
+  }, [state.succeeded, state.errors]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -35,22 +42,16 @@ function Contact() {
     }
   };
 
-  const handleSubmit = async (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
 
     if (Object.values(formData).some((value) => value.trim() === "")) {
       toast.error("Please fill in all fields!");
       return;
     }
-console.log(handleSubmit)
+
     try {
       await handleSubmitFormspree(e);
-
-      if (state.succeeded) {
-        toast.success("Form submitted successfully!");
-      } else {
-        toast.error("Error submitting form. Please try again.");
-      }
     } catch (error) {
       toast.error("Error submitting form. Please try again.");
     }
@@ -97,7 +98,7 @@ console.log(handleSubmit)
         </div>
         <div className="form flex flex-col">
           <form
-            onSubmit={handleSubmit}
+            onSubmit={handleFormSubmit}
             className="flex flex-col items-center w-full submitform"
           >
             <div className="mb-6 relative w-full ">
