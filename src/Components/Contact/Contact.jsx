2d61d67b6c0d9e62c3ddf09f8a37b97df5c6488b
Attariattari <attariattari549@gmail.com>
2024-01-21 18:32:00 +0500

    first commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -131,7 +131,7 @@ function Contact() {
                 type="text"
                 name="name"
                 placeholder={!focusedFields.name ? "Name" : ""}
-                className={`w-full outline-none pt-4 p-5${
+                className={`w-full outline-none border-b-2 border-black pt-4 p-5${
                   errors.name ? "border-red-500" : "border-gray-300"
                 }  p-4`}
                 value={formData.name}
@@ -152,7 +152,7 @@ function Contact() {
                 type="email"
                 name="email"
                 placeholder={!focusedFields.email ? "Email" : ""}
-                className={`outline-none pt-4 p-3 w-full ${
+                className={`outline-none border-b-2 border-black pt-4 p-3 w-full ${
                   errors.email ? "border-red-500" : "border-gray-300"
                 }  p-2`}
                 value={formData.email}
@@ -173,7 +173,7 @@ function Contact() {
                 type="text"
                 name="subject"
                 placeholder={!focusedFields.subject ? "subject" : ""}
-                className={`outline-none pt-4 p-3 w-full ${
+                className={`outline-none border-b-2 border-black pt-4 p-3 w-full ${
                   errors.subject ? "border-red-500" : "border-gray-300"
                 }  p-2`}
                 value={formData.subject}
@@ -193,7 +193,7 @@ function Contact() {
               <textarea
                 name="message"
                 placeholder={!focusedFields.message ? "Message" : ""}
-                className={`outline-none pt-4 p-3 w-full h-32 resize-none ${
+                className={`outline-none border-b-2 border-black pt-4 p-3 w-full h-32 resize-none ${
                   errors.message ? "border-red-500" : "border-gray-300"
                 } p-2`}
                 rows={maxRows}
